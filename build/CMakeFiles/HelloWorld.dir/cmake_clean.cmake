2a897c71ac9a8212e4a548971d82ade82c821eff
Viet Nguyen <tomnguyen168@gmail.com>
2025-05-08 20:31:47 +0700

    initialize

---
+++ build/CMakeFiles/HelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorld.dir/home/viet/projects/learn_cpp/core/src/main.cpp.o"
+  "CMakeFiles/HelloWorld.dir/home/viet/projects/learn_cpp/core/src/main.cpp.o.d"
+  "debug/HelloWorld"
+  "debug/HelloWorld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
